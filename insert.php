92346b0d3850cad3075082c22245994991f9b1ea
M303User <M303User@m303.uc-bcf.edu.ph>
2025-04-03 15:58:08 +0800

    upload php

---
+++ insert.php
@@ -0,0 +1,12 @@
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    $name = $_POST['name'];
+    $email = $_POST['email'];
+    $age = $_POST['age'];
+
+    $stmt = $conn->prepare("INSERT INTO students (name, email, age) VALUES (?, ?, ?)");
+    $stmt->bind_param("ssi", $name, $email, $age);
+    $stmt->execute();
+
+   header("Location: index.php");
+}
+?>
